23c479efa520c730ca76f6b8b77df1637ee33154
jhunter <socrates17@gmail.com>
2019-09-08 12:18:28 -0500

    remove bunch of crap from index

--- public/js/cljs-runtime/giggin.components.footer.js
+++
@@ -1,7 +0,0 @@
-goog.provide('giggin.components.footer');
-goog.require('cljs.core');
-giggin.components.footer.footer = (function giggin$components$footer$footer(){
-return new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"footer","footer",1606445390),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"img","img",1442687358),new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"src","src",-1651076051),"img/giggin-icon.png",new cljs.core.Keyword(null,"alt","alt",-3214426),"Giggin Icon"], null)], null)], null);
-});
-
-//# sourceMappingURL=giggin.components.footer.js.map
--- public/js/cljs-runtime/giggin.components.header.js
+++
@@ -1,7 +0,0 @@
-goog.provide('giggin.components.header');
-goog.require('cljs.core');
-giggin.components.header.header = (function giggin$components$header$header(){
-return new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"header","header",119441134),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"img.logo","img.logo",-1845330328),new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"src","src",-1651076051),"img/giggin-logo.svg",new cljs.core.Keyword(null,"alt","alt",-3214426),"giggin logo"], null)], null)], null);
-});
-
-//# sourceMappingURL=giggin.components.header.js.map
